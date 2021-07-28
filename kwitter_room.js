3b333a1b1ae01d6ad534a91a5ba2e17a7e79337e
Vikash36 <79023363+Vikash36@users.noreply.github.com>
2021-07-28 14:53:25 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+
+ var firebaseConfig = {
+    apiKey: "AIzaSyAnxRLiT74bkjFDYz1waelZef5Y6BGJvDQ",
+    authDomain: "let-s-chat-24746.firebaseapp.com",
+    projectId: "let-s-chat-24746",
+    storageBucket: "let-s-chat-24746.appspot.com",
+    messagingSenderId: "1010428855359",
+    appId: "1:1010428855359:web:444ed6bc068ac49d21248a"
+  };
+  firebase.initializeApp(firebaseConfig);
